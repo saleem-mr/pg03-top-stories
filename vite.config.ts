3ecc11798fb95c07c554ca9d6f80d306955fcf9a
root <root@localhost.localdomain>
2025-12-19 19:54:36 +0000

    Add GitHub Pages deployment workflow

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/pg03-top-stories/', // Required for GitHub Pages
 })
